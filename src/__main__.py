8198d9e22aebd1c42b5f9a87a0324f48608a7a5b
RamiroNB <ramiro.nilson.barros@gmail.com>
2023-03-13 23:33:27 -0300

    primeira implementação burra

--- src/__main__.py
+++ src/__main__.py
@@ -1,16 +1,43 @@
 from .monkey import Monkey
-
+#rounds.tamanhototaldaslistassomadas
 def main():
-    with open("casos/caso0200.txt") as f:
+    monkeys = []
+    monkey_r_dict = dict()
+
+    with open("casos/caso1000.txt") as f:
         lines = f.readlines()
-        #if Macaco
+        r = lines[0]
+        lines.remove(r)
+        r = r.split(" ")
+        rounds = int(r[1])
+        
         for line in lines:
-            if "\n" in line:
-                line = line.split(" ")
-                print(f"{line[0]} {line[1]}")
+            line = line.split(":")
+            referencias = line[0]
+            cocos = line [2]
+            referencias = referencias.split(" ")
+            #montar dict de referencias
+            monkey_r_dict[referencias[1]]= [referencias[4], referencias[7]]
+            #cria lista de cocos
+            cocos =cocos.strip()
+            cocos = cocos.split(" ")
 
-
-
-
+            #cria monkey
+            monkeys.append(Monkey(nome=referencias[1],cocos=cocos, par=None, inpar=None))
+        
+            #0 cria macacos
+            #1 tam vetor 
+            #numeros
+        #print(monkey_r_dict)
+        for monkey in monkeys:
+            refs =monkey_r_dict[monkey.nome]
+            monkey.par = monkeys[int(refs[0])]
+            monkey.inpar = monkeys[int(refs[1])]
+            #print(f"{monkey.nome} {monkey.par.nome} {monkey.inpar.nome}")
+    #muito tempo
+    for i in range(rounds):
+        for monkey in monkeys:
+            monkey.distribui()
+    
 if __name__ == "__main__":
     main()
--- src/monkey.py
+++ src/monkey.py
@@ -1,18 +1,23 @@
 
 class Monkey:
-    def init(self, cocos: list, par: 'Monkey',  inpar: 'Monkey'):
+    def __init__(self,nome:str, cocos: list, par: 'Monkey',  inpar: 'Monkey'):
+        self.nome = nome
         self.cocos = cocos
         self.par = par
         self.inpar = inpar
+        self.nmr_cocos = len(cocos)
  
     def distribui(self):
         for coco in self.cocos:
-            if coco % 2 == 0:
+            if int(coco) % 2 == 0:
                 self.par.recebe(coco)
             else:
                 self.inpar.recebe(coco)
-                
+
         self.cocos.clear()
+        self.nmr_cocos = len(self.cocos)
+
 
     def recebe(self, coco):
         self.cocos.append(coco)
+        self.nmr_cocos = len(self.cocos)
