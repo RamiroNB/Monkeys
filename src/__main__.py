7ecf28f83c6e589defff1a2c6419f81ba3dfc9f6
RamiroNB <ramiro.nilson.barros@gmail.com>
2023-03-14 20:19:57 -0300

    ERRADO

--- src/__main__.py
+++ src/__main__.py
@@ -6,7 +6,7 @@ def main():
     monkeys = []
     monkey_r_dict = dict()
 
-    with open("casos/caso0050.txt") as f:
+    with open("casos/caso0100.txt") as f:
         lines = f.readlines()
         r = lines[0]
         lines.remove(r)
@@ -26,28 +26,42 @@ def main():
 
             # cria monkey
             monkeys.append(
-                Monkey(nome=referencias[1], cocos=cocos, par=None, inpar=None)
+                Monkey(nome=referencias[1], cocos=cocos, par=None, impar=None)
             )
 
         for monkey in monkeys:
             refs = monkey_r_dict[monkey.nome]
             monkey.par = monkeys[int(refs[0])]
-            monkey.inpar = monkeys[int(refs[1])]
-
+            monkey.par.par_parent = monkey
+            monkey.impar = monkeys[int(refs[1])]
+            monkey.impar.impar_parent = monkey
     #########################################################################################
     print("começou")
     # muito tempo
+
     for i in range(rounds):
         [monkey.distribui() for monkey in monkeys]
+        killed_monkeys = list()
+        for j, monkey in enumerate(monkeys):
+            if (
+                len(monkey) == 0
+                and monkey.par_parent is None
+                and monkey.impar_parent is None
+            ):
+                killed_monkeys.append(j)
+                monkey.suicide()
+        # [monkeys.pop(monkey) for monkey in killed_monkeys]
+        for j, monkey_idx in enumerate(killed_monkeys):
+            monkeys.pop(monkey_idx - j)
 
     print("acabou")
     # muito tempo
     idx_vencedor = -1
     vencedor = -10
-    for i, moneky in enumerate(monkeys):
-        if len(moneky) >= vencedor:
+    for i, monkey in enumerate(monkeys):
+        if len(monkey) >= vencedor:
             idx_vencedor = i
-            vencedor = len(moneky)
+            vencedor = len(monkey)
 
     print(monkeys[idx_vencedor].nome)
     print(len(monkeys[idx_vencedor]))
@@ -57,7 +71,7 @@ if __name__ == "__main__":
     main()
 
 # ordenar as listas entre par e impar?
-# lista par e lista inpar? e fa(dimimui considz só uma vez as ordenaçõeseravelmete o nmr de op)
+# lista par e lista impar? e fa(dimimui considz só uma vez as ordenaçõeseravelmete o nmr de op)
 # distribuir os inteiros de forma paralela????
 # achar os que não são referenciados?
 # achar os que ficam em 0 depois de algumas rodadas
--- src/monkey.py
+++ src/monkey.py
@@ -1,9 +1,11 @@
 class Monkey:
-    def __init__(self, nome: str, cocos: list, par: "Monkey", inpar: "Monkey"):
+    def __init__(self, nome: str, cocos: list, par: "Monkey", impar: "Monkey"):
+        self.par_parent = None
+        self.impar_parent = None
         self.nome = nome
         self.cocos = cocos
         self.par = par
-        self.inpar = inpar
+        self.impar = impar
         self.par_list = list()
         self.impar_list = list()
         for coco in cocos:
@@ -16,12 +18,12 @@ class Monkey:
 
     def distribui(self):
         self.par.recebe_par(self.par_list)
-        self.inpar.recebe_impar(self.impar_list)
+        self.impar.recebe_impar(self.impar_list)
         # for coco in self.cocos:
         #     if int(coco) % 2 == 0:
         #         self.par.recebe(coco)
         #     else:
-        #         self.inpar.recebe(coco)
+        #         self.impar.recebe(coco)
         self.par_list.clear()
         self.impar_list.clear()
 
@@ -34,3 +36,10 @@ class Monkey:
 
     def __len__(self):
         return len(self.par_list) + len(self.impar_list)
+
+    def suicide(self):
+        self.par.par_parent = None
+        self.impar.impar_parent = None
+
+    def __str__(self) -> str:
+        return f"Monkey(Name:{self.nome}, Len:{len(self)}, ParParent:{self.par_parent if self.par_parent is None else 'another-monkey'},ImParent:{self.impar_parent if self.impar_parent is None else 'another-monkey'}, par={self.par if self.par is None else 'another-monkey'},impar={self.par if self.par is None else 'another-monkey'})"
