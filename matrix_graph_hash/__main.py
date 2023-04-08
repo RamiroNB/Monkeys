4dec7a73674b4bc112fbbb186fa20b1bbfe885ef
RamiroNB <ramiro.nilson.barros@gmail.com>
2023-04-08 18:32:16 -0300

    Add hashing dosent work

--- matrix_graph_hash/__main.py
+++ matrix_graph_hash/__main.py
@@ -1,9 +1,15 @@
-import time
+import hashlib
+
+
+def hash_matrix(matrix):
+    matrix_bytes = str(matrix).encode("utf-8")
+    matrix_hash = hashlib.sha256(matrix_bytes).hexdigest()
+    return matrix_hash
 
 
 def main():
     monkeys_r_dict = dict()
-    with open("casos/caso0050.txt") as f:
+    with open("casos/caso1000.txt") as f:
         lines = f.readlines()
         r = lines[0]
         lines.remove(r)
@@ -38,15 +44,20 @@ def main():
             monkeys[i][2] = qtd_pares
             monkeys[i][3] = qtd_impares
             i = i + 1
-
-    start = time.time()
+    old_hash = hash_matrix(monkeys)
+    idx = 0
     for round in range(rounds):
         for i in range(len(monkeys)):
             monkeys[monkeys[i][0]][2] = monkeys[monkeys[i][0]][2] + monkeys[i][2]
             monkeys[i][2] = 0
             monkeys[monkeys[i][1]][3] = monkeys[monkeys[i][1]][3] = monkeys[i][3]
             monkeys[i][3] = 0
-    end = time.time()
+        hash = hash_matrix(monkeys)
+        if hash == old_hash:
+            idx = +1
+        if idx == 5:
+            break
+
     idx_vencedor = -1
     vencedor = -10
     for i, monkey in enumerate(monkeys):
@@ -54,7 +65,6 @@ def main():
             idx_vencedor = i
             vencedor = monkey[2] + monkey[3]
 
-    print(f"game time: {end-start}")
     print(idx_vencedor)
     print(vencedor)
 
